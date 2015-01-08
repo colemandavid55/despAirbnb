08396acf371a0498b8346884e3c9962fc5e0d989
Alex Ford <alexford90@gmail.com>
2015-01-08 22:27:15 +0000

    Query for City State

--- ensnare_bnb/lib/ensnare_bnb.rb
+++ ensnare_bnb/lib/ensnare_bnb.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'open-uri'
 require 'active_support'
 require 'active_support/core_ext/hash'
+require 'pry-byebug'
 
 module EnsnareBnb
 
@@ -12,30 +13,56 @@ module EnsnareBnb
     def max_page_number(url)
       # pagination-buttons-container
       selector = "body > div.map-search > div.sidebar > " +
-                  "div.search-results.panel-body > div.results-footer > " +
+                  "div.search-results > div.results-footer > " +
                   "div.pagination-buttons-container > div.pagination > ul > " +
                   "li:nth-child(5)"
+
       Nokogiri::HTML(open(url)).css(selector).text.to_i
     end
 
     def find_airbnb_hosts(**opts)
+
+      DEFAULT_RANGE = 10
+      DEFAULT_SLEEP = 0
+
+      LAT = 0
+      LNG = 1
+
       # city, state, country
       # New-York--NY--United-States
-      city = opts.fetch(:city)
+
+      # To create new (and to remove old) logfile, add File::CREAT like:
+      file = File.open('logfile.log', File::WRONLY | File::APPEND | File::CREAT)
+      logger = Logger.new(file)
+
+      city = opts.fetch(:city, nil)
       state = opts.fetch(:state, nil)
       country = opts.fetch(:country, nil)
-      sleep_time = opts.fetch(:sleep, 1)
-
-      city_query = [city, state, country].compact.map do |str|
-        str.gsub('-', '~').gsub(' ', '-')
-      end.join('--')
-
-      search_params = opts.except(:city, :state, :country, :max_pages)
-      query = city_query + "?" + search_params.to_query
+      sw_coords = opts.fetch(:sw, nil)
+      ne_coords = opts.fetch(:ne, nil)
+      range = opts.feth(:range, DEFAULT_RANGE)
+      sleep_time = opts.fetch(:sleep, DEFAULT_SLEEP)
 
       # build intelligent options hashes/params in URLparams
 
       base_url = "https://www.airbnb.com"
+
+      city_query = "", coord_query = ""
+
+      if ( :city )
+        city_query = [city, state, country].compact.map do |str|
+          str.gsub('-', '~').gsub(' ', '-')
+        end.join('--')
+
+      elsif( :sw && :ne)
+
+        coord_query = "&sw_lat=" + sw_coords[LAT] + "&sw_lng=" + sw_coords[LNG] +
+                      "&ne_lat=" + ne_coords[LAT] + "&ne_lng=" + ne_coords[LNG]
+      end
+
+      search_params = opts.except(:city, :state, :country, :max_pages, :sw, :ne)
+      query = city_query + "?" + search_params.to_query + coord_query
+
       search_url = "#{base_url}/s/#{query}"
 
       pages = opts.fetch(:max_pages, self.max_page_number(search_url))
@@ -43,22 +70,24 @@ module EnsnareBnb
 
       pages.times do |pg|
         url = "#{search_url}?room_types%5B%5D=Entire+home%2Fapt&page=#{pg}"
-        Nokogiri::HTML(open(url)).css(".target-details.block-link").each do |box|
-          details_url = "#{base_url}#{box["href"]}"
-          inner_pg_data = Nokogiri::HTML(open(details_url))
-          name = inner_pg_data.css('#listing_name').text.strip
-          price = inner_pg_data.css('#price_amount').text[/\d+/]
-          beds = inner_pg_data.css('#summary > div > div > div.col-8 > div > div:nth-child(2) > div.col-9 > div > div:nth-child(4)').text[/\d+/]
-          bedrooms = inner_pg_data.css('#summary > div > div > div.col-8 > div > div:nth-child(2) > div.col-9 > div > div:nth-child(3)').text[/\d+/]
-          capacity = inner_pg_data.css('#summary > div > div > div.col-8 > div > div:nth-child(2) > div.col-9 > div > div:nth-child(2)').text[/\d+/]
+        Nokogiri::HTML(open(url)).css(".col-sm-12.col-md-6.row-space-2").each do |room|
+
+          name   = room.css('.listing').first['data-name']
+          lat    = room.css('.listing').first['data-lat']
+          lng    = room.css('.listing').first['data-lng']
+          url    = base_url + room.css('.listing').first['data-url']
+          price  = room.css('.price-amount').first.text
+          img    = room.css('img').first['src']
+
           output = {
             city: city,
             name: name,
             price: price.to_i,
-            bedrooms: bedrooms.to_i,
-            beds: beds.to_i,
-            capacity: capacity.to_i
-          }.to_json
+            lattitude: lat,
+            longitude: lng,
+            roomUrlExt: urlExt,
+            imgUrl: img
+          }
 
           results << output
         end
